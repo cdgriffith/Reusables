c3e4f3b9f6dac01e96337be8bba848016a0b535d
Chris Griffith <cgriffith@cyberpointllc.com>
2014-02-12 18:55:37 -0800

    Changing to name 'reuse' for now. Adding more beta items.

--- reusables/reusables_beta.py
+++ reusables/reusables_beta.py
@@ -24,3 +24,48 @@ def file_hash(path, hash_type="md5", blocksize=65536):
             hasher.update(buf)
             buf = afile.read(blocksize)
         return hasher.hexdigest()
+
+
+def reuse(func):
+    """
+    Save the variables entered into the function for reuse next time.
+
+    Not: This is a dirty function that probably shouldn't be used.
+    So when you do use it make sure to have fun with it!
+    """
+    # Could use DefaultDict but eh, it's another import
+    _reuse_cache = dict()
+    import functools
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        cache = _reuse_cache.get(func.__name__, dict(args=[], kwargs={}))
+        args = list(args)
+        local_kwargs = cache['kwargs'].copy()
+        if kwargs.get("reuse_view_cache"):
+            del kwargs["reuse_view_cache"]
+            return cache.copy()
+        if kwargs.get("reuse_del_kwargs"):
+            for x in kwargs["reuse_del_kwargs"]:
+                if x in local_kwargs['kwargs']:
+                    del local_kwargs['kwargs'][x]
+            del kwargs["reuse_del_kwargs"]
+        if kwargs.get("reuse_rep_args"):
+            for old, new in kwargs["reuse_rep_args"]:
+                if old in cache['args']:
+                    tmp_args = list(cache['args'])
+                    tmp_args[tmp_args.index(old)] = new
+                    cache['args'] = tuple(tmp_args)
+            del kwargs["reuse_rep_args"]
+        if kwargs.get("reuse_reset"):
+            cache.update(dict(args=[], kwargs={}))
+            del kwargs["reuse_reset"]
+        try:
+            args.extend(cache['args'][len(args):])
+        except IndexError:
+            pass
+        local_kwargs.update(kwargs)
+        result = func(*tuple(args), **local_kwargs)
+        _reuse_cache[func.__name__] = dict(args=tuple(args),
+                                           kwargs=local_kwargs)
+        return result
+    return wrapper
--- test/test_unit_reusables.py
+++ test/test_unit_reusables.py
@@ -2,82 +2,82 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import reusables
+import reuse
 
 
-class TestReusables(unittest.TestCase):
+class TestReuse(unittest.TestCase):
 
     def test_join_path_clean(self):
-        resp = reusables.join_paths('/test/', 'clean/', 'path')
+        resp = reuse.join_paths('/test/', 'clean/', 'path')
         assert resp == '/test/clean/path/', resp
 
     def test_join_path_dirty(self):
-        resp = reusables.join_paths('/test/', '/dirty/', ' path.file ')
+        resp = reuse.join_paths('/test/', '/dirty/', ' path.file ')
         assert resp == '/test/dirty/path.file', resp
 
     def test_join_path_clean_strict(self):
-        resp = reusables.join_paths('/test/', 'clean/', 'path/', strict=True)
+        resp = reuse.join_paths('/test/', 'clean/', 'path/', strict=True)
         assert resp == '/test/clean/path/', resp
 
     def test_join_path_dirty_strict(self):
-        resp = reusables.join_paths('/test/', '/dirty/',
+        resp = reuse.join_paths('/test/', '/dirty/',
                                     ' path.file ', strict=True)
         assert resp == '/dirty/ path.file ', resp
 
     def test_get_config_dict(self):
-        resp = reusables.config_dict('test_config.cfg')
+        resp = reuse.config_dict('test_config.cfg')
         assert resp == {'Section 1': {'key 1': 'value 1', 'key2': 'Value2'}, 'Section 2': {}}, resp
 
     def test_get_config_dict_auto(self):
-        resp = reusables.config_dict(auto_find=True)
+        resp = reuse.config_dict(auto_find=True)
         assert resp == {'Section 1': {'key 1': 'value 1', 'key2': 'Value2'}, 'Section 2': {}}, resp
 
     def test_get_config_dict_no_verify(self):
-        resp = reusables.config_dict('bad_loc.cfg', verify=False)
+        resp = reuse.config_dict('bad_loc.cfg', verify=False)
         assert resp == {}, resp
 
     def test_check_bad_filename(self):
-        resp = reusables.check_filename("safeFile?.text")
+        resp = reuse.check_filename("safeFile?.text")
         assert not resp
 
     def test_check_good_filename(self):
-        resp = reusables.check_filename("safeFile.text")
+        resp = reuse.check_filename("safeFile.text")
         assert resp
 
     def test_safe_bad_filename(self):
-        resp = reusables.safe_filename("!?!ThatsNotaFileName.\0ThatsASpaceShip^&*")
+        resp = reuse.safe_filename("!?!ThatsNotaFileName.\0ThatsASpaceShip^&*")
         assert not [x for x in ("!", "?", "\0", "^", "&", "*") if x in resp], resp
-        assert reusables.check_filename(resp)
+        assert reuse.check_filename(resp)
 
     def test_safe_good_filename(self):
         infilename = "SafeFile.txt"
-        resp = reusables.safe_filename(infilename)
+        resp = reuse.safe_filename(infilename)
         assert resp == infilename, resp
 
     def test_safe_bad_path(self):
         path = "/var/lib\\/test/p?!ath/fi\0lename.txt"
         expected = "/var/lib_/test/p__ath/fi_lename.txt"
-        resp = reusables.safe_path(path)
+        resp = reuse.safe_path(path)
         assert not [x for x in ("!", "?", "\0", "^", "&", "*") if x in resp], resp
         assert resp == expected, resp
 
     def test_safe_good_path(self):
         path = "/var/lib/test/path/filename.txt"
-        resp = reusables.safe_path(path)
+        resp = reuse.safe_path(path)
         assert resp == path, resp
 
     def test_main(self):
-        reusables._main(True)
+        reuse._main(True)
 
     def test_sorting(self):
         al = [{"name": "a"}, {"name": "c"}, {"name": "b"}]
-        resp = reusables.sort_by(al, "name")
+        resp = reuse.sort_by(al, "name")
         assert resp[0]['name'] == 'a'
         assert resp[1]['name'] == 'b'
         assert resp[2]['name'] == 'c'
 
     def test_type_errors(self):
-        self.assertRaises(TypeError, reusables.config_dict, dict(config='1'))
-        self.assertRaises(TypeError, reusables.check_filename, tuple())
-        self.assertRaises(TypeError, reusables.safe_filename, set())
-        self.assertRaises(TypeError, reusables.safe_path, dict())
+        self.assertRaises(TypeError, reuse.config_dict, dict(config='1'))
+        self.assertRaises(TypeError, reuse.check_filename, tuple())
+        self.assertRaises(TypeError, reuse.safe_filename, set())
+        self.assertRaises(TypeError, reuse.safe_path, dict())
