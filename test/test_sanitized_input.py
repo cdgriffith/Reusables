4a1c6c75fc163412433b132458d72322981f4a0c
Dogeek <Dogeek@users.noreply.github.com>
2019-07-16 13:03:19 +0200

    more tests

--- test/test_sanitized_input.py
+++ test/test_sanitized_input.py
@@ -6,8 +6,17 @@ import unittest.mock as mock
 
 from .common_test_data import *
 
-def input(message, index=0):
-    return ["0", "1", "a"][max(min)]
+class TestException(Exception):
+    pass
+
+
+class IntVar:
+    def __init__(self, value):
+        if isinstance(value, str):
+            self.value = int(value)
+        else:
+            raise TestException("Exception")
+
 
 class TestSanitizedInput(BaseTestClass):
 
@@ -32,6 +41,26 @@ class TestSanitizedInput(BaseTestClass):
         with mock.patch('builtins.input', return_value="32"):
             self.assertEqual(32,
                              reusables.sanitized_input(**kwargs))
+
+    def test_cast_obj(self):
+        kwargs = {"message": "",
+                  "cast_obj": IntVar,
+                  "n_retries": -1,
+                  "error_msg": "",
+                  "valid_input": [],
+                  "raise_on_invalid": False}
+        with mock.patch('builtins.input', return_value=1):
+            self.assertRaises(TestException,
+                              reusables.sanitized_input, **kwargs)
+        with mock.patch('builtins.input', return_value='1'):
+            assert isinstance(reusables.sanitized_input(cast_obj=IntVar), IntVar), "Success"
+            assert not isinstance(reusables.sanitized_input(cast_obj=IntVar), int), "Failure"
+
+    def test_cast_successful(self):
+        with mock.patch('builtins.input', return_value='1'):
+            assert isinstance(reusables.sanitized_input(cast_obj=int), int), 'Success'
+            assert not isinstance(reusables.sanitized_input(cast_obj=str), int), 'Failure'
+            assert isinstance(reusables.sanitized_input(cast_obj=str), str), 'Success'
 
     def test_valid_input(self):
         kwargs = {"message": "",
