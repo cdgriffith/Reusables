e033acb5ee11a8a3dcc7a4db2781f92ce95c5743
Chris Griffith <chris@cdgriffith.com>
2019-07-16 19:32:12 -0500

    Documentation cleanup

--- reusables/sanitizers.py
+++ reusables/sanitizers.py
@@ -21,10 +21,10 @@ def _get_input(prompt):
 def sanitized_input(message="", cast_as=None, number_of_retries=-1,
                     error_message="", valid_input=(), raise_on_invalid=False):
     """
-         Function sanitized_input :
-         @args
-             message: string to show the user (default: "")
-             cast_as: an object to cast the string into. Object must have a __new__
+        Clean up and cast user input.
+
+        :param message: string to show the user (default: "")
+        :param cast_as: an object to cast the string into. Object must have a __new__
                        method that can take a string as the first positional argument
                        and be a subclass of type.
                        The object should raise a ValueError exception if a
@@ -32,25 +32,29 @@ def sanitized_input(message="", cast_as=None, number_of_retries=-1,
                        cast_obj can also be a tuple or a list, which will
                        chain casts until the end of the list. Casts are chained in
                        reverse order of the list (to mimic the syntax int(float(x))) (default: str)
-             number_of_retries: number of retries. No limit if n_retries == -1 (default: -1)
-             error_message: message to show the user before asking the input again in
+        :param number_of_retries: number of retries. No limit if n_retries == -1 (default: -1)
+        :param error_message: message to show the user before asking the input again in
                             case an error occurs (default: repr of the exception).
                             Can include {error}.
-             valid_input: an iterable to check if the result is allowed.
-             raise_on_invalid: boolean, whether this function will raise a
+        :param valid_input: an iterable to check if the result is allowed.
+        :param raise_on_invalid: boolean, whether this function will raise a
                                reusables.InvalidInputError if the input doesn't match
                                the valid_input argument.
-        @returns
-            rv : string literal casted into the cast_obj as per that object's rules.
-            raises : RetryCountExceededError if the retry count has exceeded the n_retries limit.
-        @examples
+        :return: string literal casted into the cast_obj as per that object's rules.
+
+        :raises: RetryCountExceededError if the retry count has exceeded the n_retries limit.
+
+
+        Examples:
             integer = sanitized_input("How many apples?", int,
                       error_msg="Please enter a valid number")
-                >>> returns an int, will prompt until the user enters an integer.
+            # returns an int, will prompt until the user enters an integer.
+
             validated = sanitized_input(">>>", valid_input=["string"], raise_on_invalid=True)
-                >>> returns the value "string", and will raise InvalidInputError otherwise.
+            # returns the value "string", and will raise InvalidInputError otherwise.
+
             chain_cast = sanitized_input(">>>", cast_obj=[int, float])
-                >>> returns an int, prompts like '2.3' won't raise a ValueError Exception.
+            # returns an int, prompts like '2.3' won't raise a ValueError Exception.
     """
     retry_count = 0
 
